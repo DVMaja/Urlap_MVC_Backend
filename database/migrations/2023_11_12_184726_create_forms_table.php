0ded2a4b0dc8c15e1e167090eddd026f11f6ee0e
DVMaja <majadreilinger@gmail.com>
2023-11-13 22:09:44 +0100

    migrációs fájl ok, fillable megvan

--- database/migrations/2023_11_12_184726_create_forms_table.php
+++ database/migrations/2023_11_12_184726_create_forms_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,8 +14,15 @@ return new class extends Migration
     {
         Schema::create('forms', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nev');
+            $table->integer('szul_ev');
+            //$table->timestamps();
         });
+
+        DB::table('forms')->insert([
+            ['nev'=> 'Proba Ákos','szul_ev'=> '2000'],
+            ['nev'=> 'Proba Ági','szul_ev'=> '1999'],
+        ]);
     }
 
     /**
